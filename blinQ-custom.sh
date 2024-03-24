7fdeebb953acf7804bad74cefea7cecd525e071f
Hein Tonny Køien <hein.koien@blinq.no>
2024-03-24 08:15:50 +0100

    Improve blinQ Custom to support Azure Key Vault ref. for GitHub App Private Key

--- blinQ-custom.sh
+++ blinQ-custom.sh
@@ -1,8 +1,15 @@
 # --------------  blinQ Custom Entry Point Start -------------------------
 # Add support for Azure Key Vault Secret ref for GitHub App Private Key
 # Source the helper functions
-source ./blinQ-functions.sh
-# Check for required dependencies and execute the main function
-check_dependencies
-runner_token
+if [[ -z "$APP_PRIVATE_KEY"]];then
+  source ./blinQ-functions.sh
+  fetch_azure_access_token
+  get_private_key_from_azure_keyvault
+  if [[ -n "$PRIVATE_KEY" ]];then
+    APP_PRIVATE_KEY=$PRIVATE_KEY
+  else
+    echo "Failed to get APP_PRIVATE_KEY from Azure Key Vault"
+    exit 1
+  fi  
+fi
 # --------------  blinQ Custom Entry Point End --------------------------
--- blinQ-functions.sh
+++ blinQ-functions.sh
@@ -3,8 +3,8 @@ function check_dependencies() {
     # Always check for openssl, jq, and curl.
     local dependencies=("openssl" "jq" "curl")
     
-    # Check for az cli if MSI_ID is provided and not MSI_CLIENT_ID.
-    if [[ -n "${MSI_ID}" ]] && [[ -z "${MSI_CLIENT_ID}" ]]; then
+    # Check for az cli if AZURE_MSI_ID is provided and not AZURE_MSI_ID.
+    if [[ -n "${AZURE_MSI_ID}" ]] && [[ -z "${AZURE_MSI_ID}" ]]; then
         dependencies+=("az")
     fi
 
@@ -16,11 +16,6 @@ function check_dependencies() {
     done
 }
 
-# Encode input to base64 URL safe format.
-function b64enc() { 
-    openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n';  # Remove padding, replace URL-unsafe chars, remove newlines.
-}
-
 # Fetches the access token from Azure Instance Metadata Service (IMDS) using either system-assigned or user-assigned managed identity.
 function fetch_access_token_from_imds() {
     echo "Fetch access token from IMDS"
@@ -30,9 +25,9 @@ function fetch_access_token_from_imds() {
     
     # Construct the request URL. Include client_id if using user-assigned managed identity.
     local request_url
-    if [[ -n "${MSI_CLIENT_ID}" ]]; then
-        echo "Using user-assigned managed identity ${MSI_CLIENT_ID}"
-        local encoded_client_id=$(printf '%s' "${MSI_CLIENT_ID}" | jq -sRr @uri)  # URL-encode the client_id
+    if [[ -n "${AZURE_MSI_ID}" ]]; then
+        echo "Using user-assigned managed identity ${AZURE_MSI_ID}"
+        local encoded_client_id=$(printf '%s' "${AZURE_MSI_ID}" | jq -sRr @uri)  # URL-encode the client_id
         request_url="${token_endpoint}?resource=${resource}&${api_version}&client_id=${encoded_client_id}"
     else
         echo "Using system-assigned managed identity"
@@ -46,9 +41,9 @@ function fetch_access_token_from_imds() {
         cat temp.json  # Output error response for diagnostics.
         exit 1
     fi
-    ACCESS_TOKEN=$(jq -r '.access_token' temp.json)
+    AZURE_ACCESS_TOKEN=$(jq -r '.access_token' temp.json)
     rm temp.json  # Clean up temporary file.
-    if [[ -z "$ACCESS_TOKEN" ]]; then
+    if [[ -z "$AZURE_ACCESS_TOKEN" ]]; then
         echo "Failed to fetch access token."
         exit 1
     else
@@ -60,15 +55,15 @@ function fetch_access_token_from_imds() {
 function fetch_access_token_from_az_cli() {
     # Use Azure CLI to obtain an access token for the Key Vault.
     echo "Fetch access token from Azure CLI"
-    if [[ -n "${MSI_ID}" ]]; then
-        echo "Using user-assigned managed identity ${MSI_ID}"
-        az login --identity -u "${MSI_ID}"  # Login with user-assigned managed identity.
+    if [[ -n "${AZURE_MSI_ID}" ]]; then
+        echo "Using user-assigned managed identity ${AZURE_MSI_ID}"
+        az login --identity -u "${AZURE_MSI_ID}"  # Login with user-assigned managed identity.
     else
         echo "Using system-assigned managed identity"
         az login --identity  # Login with system-assigned managed identity.
     fi
-    ACCESS_TOKEN=$(az account get-access-token --resource https://vault.azure.net --query accessToken -o tsv)
-    if [[ -z "$ACCESS_TOKEN" ]]; then
+    AZURE_ACCESS_TOKEN=$(az account get-access-token --resource https://vault.azure.net --query accessToken -o tsv)
+    if [[ -z "$AZURE_ACCESS_TOKEN" ]]; then
         echo "Failed to fetch access token."
         exit 1
     else
@@ -76,129 +71,34 @@ function fetch_access_token_from_az_cli() {
     fi
 }
 
-# Generates JWT header and payload for GitHub App authentication.
-function jwt_header_payload() {
-    echo "Generate JWT header and payload"
-    local header_json='{ "typ":"JWT", "alg":"RS256" }'
-    header=$( echo -n "${header_json}" | b64enc )
-    local now=$(date +%s)
-    local iat=$((${now} - 60))
-    local exp=$((${now} + 600))
-    local payload_json='{ "iat":'"${iat}"', "exp":'"${exp}"', "iss":'"${GH_APP_ID}"' }'
-    payload=$( echo -n "${payload_json}" | b64enc )
-    header_payload="${header}.${payload}"  # Concatenate header and payload.
+# Fetch Azure Access Token, prefer imds before az cli
+function fetch_azure_access_token() {
+  if [[ -n "$AZURE_MSI_ID"]];then
+  # Authenticate with userassigned managed identity
+    # Check if AZURE_MSI_ID contains a client_id, ex: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx or a managed identity resource id, ex: /subscriptions/xxxxxxx/resourceGroups/xxxxxxx/providers/Microsoft.ManagedIdentity/userAssignedIdentities/xxxxxxx
+    resource_id = $(echo $AZURE_MSI_ID | grep Microsoft.ManagedIdentity)
+    if [[ -n "$resource_id" ]]; then
+      # If AZURE_MSI_ID contains resource_id we use imds to fetch azure access token
+      fetch_access_token_from_imds
+    else
+      # If AZURE_MSI_ID not contains resource_id we assume it's a client_id, we use az cli to fetch access token
+      fetch_access_token_from_az_cli
+    fi
+  else
+  # Authenticate with systemassigned managed identity using imds
+    fetch_access_token_from_imds
+  fi  
 }
 
 # Signs the JWT using the private key.
-function jwt_signature_private_key() {
-    if [[ -z "$GH_APP_PRIVATE_KEY_BASE64" ]]; then
-        echo "Private key not provided as input; attempting to download from Azure Key Vault..."
-        local secret=$(curl -s -H "Authorization: Bearer ${ACCESS_TOKEN}" "https://${AZURE_KEYVAULT_NAME}.vault.azure.net/secrets/${AZURE_KEYVAULT_SECRET}?api-version=7.1")
+function get_private_key_from_azure_keyvault() {
+    if [[ -z "$APP_PRIVATE_KEY" ]] && [[ -n "$AZURE_KEYVAULT_NAME" ]] && [[ -n "$AZURE_KEYVAULT_SECRET" ]]; then
+        echo "GitHub App Private key not provided as input; attempting to download from Azure Key Vault..."
+        local secret=$(curl -s -H "Authorization: Bearer ${AZURE_ACCESS_TOKEN}" "https://${AZURE_KEYVAULT_NAME}.vault.azure.net/secrets/${AZURE_KEYVAULT_SECRET}?api-version=7.1")
         PRIVATE_KEY=$(echo $secret | jq -r .value)
         if [[ -z "$PRIVATE_KEY" ]]; then
             echo "Failed to download private key from Azure Key Vault."
             exit 1
         fi
-    else
-      # Decode GH_APP_PRIVATE_KEY_BASE64
-      PRIVATE_KEY=$(echo $GH_APP_PRIVATE_KEY_BASE64 | base64 -d)
     fi
-    echo -n "$PRIVATE_KEY" > private.pem
-    if [ ! -f private.pem ]; then
-        echo "Failed to create PEM file from private key."
-        exit 1
-    fi
-    SIGNATURE=$(echo -n "${header_payload}" | openssl dgst -sha256 -sign private.pem | b64enc)
-    rm private.pem  # Ensure the private key file is removed after use
-}
-
-
-# Main function to get the GitHub runner registration token.
-function runner_token() {
-  if [[ -n "${RUNNER_TOKEN}" ]]; then
-    echo "Connect to GitHub using RUNNER_TOKEN environment variable."
-  elif [[ -n "${GH_APP_ID}" ]] && [[ -n "${GH_OWNER}" ]]; then
-    # Generate the JWT header and payload.
-    jwt_header_payload  
-  
-    # Sign the JWT header and payload with private key
-    jwt_signature_private_key
-
-    # Handle failure in JWT signing.
-    if [[ -z "$SIGNATURE" ]] || [[ "$SIGNATURE" == "null" ]]; then
-        echo "Failed to sign the header and payload."
-        exit 1
-    else
-      echo "Header and payload signed successfully"
-      # Construct the JWT by concatenating the encoded header, payload, and signature.
-      JWT="${header_payload}.${SIGNATURE}"
-    fi
-
-    # Fetch the GitHub App installation ID and subsequently the installation token.
-    # This token is used to request a runner registration token for the specified organization or repository.
-    # Additional checks ensure the validity of the obtained tokens.
-
-    # Start to get the installation token and agent registration token
-    if [[ -n "${JWT}" ]]; then
-      echo "JWT generated successfully"
-      # Get the installation ID for the organization
-      if [[ -z "${GH_APP_INSTALLATION_ID}" ]]; then
-        if [[ -z "${GH_REPOSITORY}" ]]; then
-          echo "Get installation ID for the organization"
-          GH_APP_INSTALLATION_ID=$(curl -s -H "Authorization: Bearer $JWT" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/app/installations" | jq '.[] | select(.account.login=="'$ORG'") | .id')
-        # Get the installation ID for the repository
-        else
-          echo "Get installation ID for the repository"
-          GH_APP_INSTALLATION_ID=$(curl -s -H "Authorization: Bearer $JWT" -H "Accept: application/vnd.github.v3+json" \
-            "https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/installation" | jq -r .id)
-        fi
-        echo "GH_APP_INSTALLATION_ID: ${GH_APP_INSTALLATION_ID}"
-      else
-        echo "Using provided installation ID: ${GH_APP_INSTALLATION_ID}"
-      fi
-      
-      # Check for valid installation ID
-      if [[ -z "$GH_APP_INSTALLATION_ID" ]] || [[ "$GH_APP_INSTALLATION_ID" == "null" ]]; then
-          echo "Failed to get installation ID."
-          exit 1
-      else
-        # Get installation access token
-        echo "Get installation access token"
-        INSTALLATION_TOKEN=$(curl -sX POST -H "Authorization: Bearer $JWT" -H "Accept: application/vnd.github.v3+json" \
-          "https://api.github.com/app/installations/${GH_APP_INSTALLATION_ID}/access_tokens" | jq -r .token)
-
-        # Check for valid installation token
-        if [[ -z "$INSTALLATION_TOKEN" ]] || [[ "$INSTALLATION_TOKEN" == "null" ]]; then
-            echo "Failed to get installation token."
-            exit 1
-        else
-          echo "Got installation token"
-          # Get agent registration token for the org / repository
-          if [[ -z "${GH_REPOSITORY}" ]]; then
-            # Org level runner
-            echo "Get agent registration token for the organization"
-            RUNNER_TOKEN=$(curl -s POST -H "Authorization: token ${INSTALLATION_TOKEN}" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/orgs/${ORG}/actions/runners/registration-token" | jq -r .token)
-          else
-            # Repo level runner
-            echo "Get agent registration token for the repository"
-            RUNNER_TOKEN=$(curl -sX POST -H "Authorization: token ${INSTALLATION_TOKEN}" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token" | jq -r .token)
-          fi
-          # Check for valid agent token
-          if [[ -z "$RUNNER_TOKEN" ]] || [[ "$RUNNER_TOKEN" == "null" ]]; then
-              echo "Failed to get agent registration token."
-              exit 1
-          else
-            echo "Got agent registration token"
-            export RUNNER_TOKEN=$RUNNER_TOKEN
-          fi          
-        fi
-      fi
-    fi
-  else
-    echo "Required environment variables are missing. Please provide one of the following sets:"
-    echo " - GH_OWNER, GH_APP_ID, AZURE_KEYVAULT_NAME, and AZURE_KEYVAULT_SECRET (Recommended)"
-    echo " - GH_OWNER, GH_APP_ID, and GH_APP_PRIVATE_KEY_BASE64 (Not recommended)"
-    echo " - MSI_ID or MSI_CLIENT_ID if you use user assigned managed identity"
-    exit 1
-  fi
 }
