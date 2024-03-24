3671b5f385e14954448cf331dfeb35be533fb766
Hein Tonny Køien <hein.koien@blinq.no>
2024-03-24 11:31:58 +0100

    Support AZURE_ACCESS_TOKEN as an optional input to simplify local debug

--- blinQ-custom.sh
+++ blinQ-custom.sh
@@ -3,7 +3,9 @@
 # Source the helper functions
 if [[ -n "$AZURE_KEYVAULT_NAME" ]] && [[ -n "$AZURE_KEYVAULT_SECRET" ]] && [[ -n "$APP_ID" ]] && [[ -z "$APP_PRIVATE_KEY" ]];then  
   source /blinQ-functions.sh
-  fetch_access_token_from_imds
+  if [[ -z "$AZURE_ACCESS_TOKEN" ]];then
+    fetch_access_token_from_imds
+  fi
   get_private_key_from_azure_keyvault
   if [[ -n "$PRIVATE_KEY" ]];then
     PRIVATE_KEY_MULTILINE=$(echo -e "${PRIVATE_KEY}")
