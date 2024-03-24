c066007854df62e785937e4645a1ed039275276c
Hein Tonny Køien <hein.koien@blinq.no>
2024-03-24 12:33:11 +0100

    Fix azure access token fetching

--- blinQ-functions.sh
+++ blinQ-functions.sh
@@ -1,43 +1,18 @@
 # Fetches the access token from Azure Instance Metadata Service (IMDS) using either system-assigned or user-assigned managed identity.
 function fetch_access_token_from_imds() {
-    echo "Fetch access token from IMDS"
-    local resource="https://vault.azure.net"
-    local token_endpoint="http://169.254.169.254/metadata/identity/oauth2/token"
-    local api_version="api-version=2018-02-01"
-    
-    # Construct the request URL. Include client_id if using user-assigned managed identity.
-    local request_url
-    if [[ -n "${AZURE_MSI_ID}" ]]; then
-        echo "Using user-assigned managed identity ${AZURE_MSI_ID}"
-        local encoded_client_id=$(printf '%s' "${AZURE_MSI_ID}" | jq -sRr @uri)  # URL-encode the client_id
-        request_url="${token_endpoint}?resource=${resource}&${api_version}&client_id=${encoded_client_id}"
+    if [[ -z "$AZURE_MSI_ID" ]]; then
+      local client_id="$AZURE_MSI_ID"
+      AZURE_ACCESS_TOKEN=$(curl "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net&client_id=$client_id" -H Metadata:true -s | jq -r '.access_token')
     else
-        echo "Using system-assigned managed identity"
-        request_url="${token_endpoint}?resource=${resource}&${api_version}"
-    fi
-
-    # Fetch the token and check for successful retrieval.
-    local response=$(curl -H "Metadata: true" -s -w "%{http_code}" -o temp.json "${request_url}")
-    if [ "$response" -ne 200 ]; then
-        echo "Failed to fetch access token, HTTP status code: $response."
-        cat temp.json  # Output error response for diagnostics.
-        exit 1
-    fi
-    AZURE_ACCESS_TOKEN=$(jq -r '.access_token' temp.json)
-    rm temp.json  # Clean up temporary file.
-    if [[ -z "$AZURE_ACCESS_TOKEN" ]]; then
-        echo "Failed to fetch access token."
-        exit 1
-    else
-      echo "Access token fetched successfully"
+      AZURE_ACCESS_TOKEN=$(curl "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net" -H Metadata:true -s | jq -r '.access_token')
     fi
 }
 
 # Download the private key from Azure Key Vault using the provided secret name.
 function get_private_key_from_azure_keyvault() {
     if [[ -z "$APP_PRIVATE_KEY" ]] && [[ -n "$AZURE_KEYVAULT_NAME" ]] && [[ -n "$AZURE_KEYVAULT_SECRET" ]]; then
-        echo "GitHub App Private key not provided as input; attempting to download from Azure Key Vault..."
-        local secret=$(curl -s -H "Authorization: Bearer ${AZURE_ACCESS_TOKEN}" "https://${AZURE_KEYVAULT_NAME}.vault.azure.net/secrets/${AZURE_KEYVAULT_SECRET}?api-version=7.1")
+        echo "GitHub App Private key not provided as input; attempting to download from Azure Key Vault..."                       
+        local secret=$(curl  https://${AZURE_KEYVAULT_NAME}.vault.azure.net/secrets/${AZURE_KEYVAULT_SECRET}/?api-version=7.4 -H "Authorization: Bearer $AZURE_ACCESS_TOKEN")
         PRIVATE_KEY=$(echo $secret | jq -r .value)
         if [[ -z "$PRIVATE_KEY" ]]; then
             echo "Failed to download private key from Azure Key Vault."
